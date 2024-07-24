de41f43f73f7311845b5efe6a91bb038c117a981
unknown <bgsdewoo20@gmail.com>
2024-07-24 13:10:07 +0700

    Menggunakan On Demand Revalidation

--- app/webhooks/cms/route.js
+++ app/webhooks/cms/route.js
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const payload = await request.json();
-  console.log("payload", payload);
+  if (payload.model === "post") {
+    revalidateTag(CACHE_TAG_POSTS);
+    console.log("revalidated:", CACHE_TAG_POSTS);
+  }
   return new Response(null, { status: 204 });
 }
