b4f102cff0a79346f5498a6761f7390222fecc1b
unknown <bgsdewoo20@gmail.com>
2024-07-24 13:33:42 +0700

    Mendapatkan Data Dari Query Parameter Untuk Pagination

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,18 +1,25 @@
 import Heading from "@/components/Heading";
 import PostCard from "@/components/PostCard";
 import { getAllPosts } from "@/lib/post";
+import Link from "next/link";
 export const revalidate = 30;
 
 export const metadata = {
   title: "Blog",
 };
-export default async function BlogPage() {
+export default async function BlogPage({ searchParams }) {
+  const page = parsePageParam(searchParams.page);
   const posts = await getAllPosts();
 
   return (
     <>
       <Heading>Blog</Heading>
       <h2 className="text-2xl mb-3">List of posts</h2>
+      <div className="flex gap-3 pb-3">
+        <Link href={`/blog?page=${page - 1}`}>&lt;</Link>
+        <span>Page {page}</span>
+        <Link href={`/blog?page=${page + 1}`}>&gt;</Link>
+      </div>
       {posts.map((post, index) => (
         <PostCard
           key={index}
@@ -26,4 +33,14 @@ export default async function BlogPage() {
       ))}
     </>
   );
+}
+
+function parsePageParam(paramValue) {
+  if (paramValue) {
+    const page = parseInt(paramValue);
+    if (isFinite(page) && page > 0) {
+      return page;
+    }
+  }
+  return 1;
 }
